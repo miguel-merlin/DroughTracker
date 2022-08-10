7a92e1317254a52cf594273fb43c6fb8189b6ba0
miguel-merlin <mmerlin@stevens.edu>
2022-08-10 18:12:56 -0500

    Home page

--- client/src/Components/CarouselDrought.jsx
+++ client/src/Components/CarouselDrought.jsx
@@ -1,31 +1,48 @@
 import Carousel from 'react-bootstrap/Carousel';
+import Card from 'react-bootstrap/Card';
+import logo from '../Assets/imgs/logo.svg'
 import img1 from '../Assets/imgs/drought1.jpg';
 import img2 from '../Assets/imgs/drought2.jpg';
 import img3 from '../Assets/imgs/drought3.jpg';
 
 const CarouselDrought = () => {
-    return(
+    return (
         <Carousel fade>
             <Carousel.Item>
                 <img
-                className='d-block w-100'
-                src={img1}
-                alt="Droughts"
+                    className='d-block w-100'
+                    src={img1}
+                    alt="Droughts"
                 />
+                <Carousel.Caption>
+                    <Card>
+                        <Card.Img src={logo} />
+                    </Card>
+                </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
-                className='d-block w-100'
-                src={img2}
-                alt="Droughts"
+                    className='d-block w-100'
+                    src={img2}
+                    alt="Droughts"
                 />
+                <Carousel.Caption>
+                    <Card>
+                        <Card.Img src={logo} />
+                    </Card>
+                </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
-                className='d-block w-100'
-                src={img3}
-                alt="Droughts"
+                    className='d-block w-100'
+                    src={img3}
+                    alt="Droughts"
                 />
+                <Carousel.Caption>
+                    <Card>
+                        <Card.Img src={logo} />
+                    </Card>
+                </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
     )
--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import AppBar from "../Components/AppBar"
 import CarouselDrought from '../Components/CarouselDrought';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 const Home = () => {
     return (
         <>
             <AppBar />
-            <CarouselDrought />
+            <Container>
+                <Row>
+                    <Col/>
+                    <Col xs={10}>
+                        <CarouselDrought />
+                    </Col>
+                    <Col/>
+                </Row>
+            </Container>
         </>
     )
 }
